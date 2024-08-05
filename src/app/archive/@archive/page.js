d138b207341935eaf7cc8e0e90c1d9d3cefdc9e2
ahmadowais-dev <ahmad.owais@intravar.com>
2024-08-05 15:45:57 +0500

    parallel routing basic layout

---
+++ src/app/archive/@archive/page.js
@@ -0,0 +1,7 @@
+export default function ArchivePage() {
+    return <div>
+        <h1>This is archive page</h1>
+    </div>
+
+
+}
---
+++ src/app/archive/@latest/page.js
@@ -0,0 +1,5 @@
+export default function LatestNewsPage() {
+    return <div>
+        <h1>This is latest news page</h1>
+    </div>
+}
